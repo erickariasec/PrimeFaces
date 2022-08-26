2710343b49425a42744d8ab02cdc39d7e31c2ce5
erickariasec <erickarias1999@hotmail.com>
2022-08-25 23:46:35 -0500

    PrimeFaces, PrimeReact, prime-react-app, src, App.js, adding some basic prime react to see how to use this

--- PrimeReact/prime-react-app/src/App.js
+++ PrimeReact/prime-react-app/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+import { useState } from 'react';
+ 
+
 function App() {
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <InputText value={text} onChange={e => setText(e.target.value)} />
+        { text }
+
+        <br /><br />
+
+        <Button type='button' label='Submit' icon='pi pi-check'></Button>
       </header>
     </div>
   );
